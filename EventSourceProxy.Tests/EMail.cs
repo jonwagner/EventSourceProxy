3fa50233ba0a81817d5faa74bde59fa60e46e187
Ian Bebbington <github@bebbs.co.uk>
2013-10-18 16:19:24 +0100

    EventDataProvider example
    Provides an example "EventDataProvider" along with tests which show how
    this class could be constructed/used.

---
+++ EventSourceProxy.Tests/EMail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourceProxy.Tests
+{
+    public interface IEmail
+    {
+        string From { get; }
+        string To { get; }
+        string Subject { get; }
+        string Body { get; }
+    }
+
+    public class EMail : IEmail
+    {
+        public EMail(string from, string to, string subject, string body)
+        {
+            From = from;
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+
+        public string From { get; private set; }
+        public string To { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+    }
+}
---
+++ EventSourceProxy/EventDataProvider.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourceProxy
+{
+    public interface IEventDataProvider<T>
+    {
+        IEnumerable<Tuple<string, object>> GetPayloadFrom(T source);
+
+        IEnumerable<Tuple<string, Type>> Schema { get; }
+    }
+
+    public interface IEventValueProvider<in TSource>
+    {
+        string Name { get; }
+
+        Type Type { get; }
+
+        object GetValue(TSource source);
+    }
+
+    internal class InspectingValueProvider<TSource, TValue> : IEventValueProvider<TSource>
+    {
+        private readonly Func<TSource, TValue> _func;
+
+        public InspectingValueProvider(Expression<Func<TSource, TValue>> expression)
+        {
+            Name = expression.GetMemberInfo().Name;
+            Type = typeof(TValue);
+
+            _func = expression.Compile();
+        }
+
+        public string Name { get; private set; }
+
+        public Type Type { get; private set; }
+
+        public object GetValue(TSource source)
+        {
+            return _func(source);
+        }
+    }
+
+    public class EventDataProvider<TSource> : IEventDataProvider<TSource>
+    {
+        private readonly IEventValueProvider<TSource>[] _valueProviders;
+
+        /// <summary>
+        /// Constructs a <see cref="IEventValueProvider{T}"/> for the <see cref="TSource"/> which uses the
+        /// specified <see cref="Expression{T}"/> of <see cref="Func{T,T}"/> to return a schema/payload information
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static IEventValueProvider<TSource> Inspect<TValue>(Expression<Func<TSource, TValue>> expression)
+        {
+            return new InspectingValueProvider<TSource, TValue>(expression);
+        }
+
+        /// <summary>
+        /// Instantiates a new InspectingEventDataProvider that uses the specified <s
+        /// </summary>
+        /// <param name="valueProviders"></param>
+        public EventDataProvider(params IEventValueProvider<TSource>[] valueProviders)
+        {
+            _valueProviders = valueProviders;
+        }
+
+        /// <summary>
+        /// Extracts payload values from the specified <typeparamref name="TSource"/> value
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns>An unmaterlizard <see cref="IEnumerable{T}"/> of <see cref="Tuple{T,T}"/></returns>
+        /// <remarks>
+        /// This method deliberately returns an unmaterialized LINQ expression to ensure minimal overhead while
+        /// tracing is not enabled for a type
+        /// </remarks>
+        public IEnumerable<Tuple<string, object>> GetPayloadFrom(TSource source)
+        {
+            return _valueProviders.Select(provider => Tuple.Create(provider.Name, provider.GetValue(source)));
+        }
+
+        /// <summary>
+        /// Returns schema information for the <see cref="EventDataProvider{T}"/>
+        /// </summary>
+        /// <returns>An unmaterialized <see cref="IEnumerable{T}"/> of <see cref="Tuple{T,T}"/></returns>
+        /// <remarks>
+        /// This method deliberately returns an unmaterialized LINQ expression to ensure minimal overhead while
+        /// tracing is not enabled for a type
+        /// </remarks>
+        public IEnumerable<Tuple<string, Type>> Schema
+        {
+            get { return _valueProviders.Select(provider => Tuple.Create(provider.Name, provider.Type)); }
+        }
+    }
+}
